402af854ca6af718326191b5afebb8f0c7cb9ca0
CodePoetPuls <2661235662@qq.com>
2025-07-13 07:08:11 +0800

    Delete js directory

--- js/index.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.bg-primary').addEventListener('click', () => {
-    document.querySelector('.bg-primary').style.backgroundColor = 'red';
-    location.replace("/mangshi_Ruili_Day_Trip.html");
-});
--- js/mangshi_Ruili_Day_Trip.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.fas').addEventListener('click', () => {
-    location.replace("/index.html");
-});
