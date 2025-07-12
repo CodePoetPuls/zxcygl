f39be3e033bbb269a53d42e47531f42eccfb4a0e
CodePoetPuls <2661235662@qq.com>
2025-07-13 05:35:23 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.bg-primary').addEventListener('click', () => {
+    document.querySelector('.bg-primary').style.backgroundColor = 'red';
+    location.replace("/mangshi_Ruili_Day_Trip.html");
+});
