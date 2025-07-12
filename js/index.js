741a131281ccf237176bfd313f1a3ab229896940
CodePoetPuls <2661235662@qq.com>
2025-07-13 07:09:54 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// 瑞丽芒市一日游跳转效果+跳转功能
+document.querySelector('#button_a').addEventListener('click', () => {
+    document.querySelector('#button_a').style.backgroundColor = 'red';
+    location.replace("/mangshi_Ruili_Day_Trip.html");
+});
+//瑞丽芒市两日游跳转效果+跳转功能
+document.querySelector('#button_b').addEventListener('click', () => {
+    document.querySelector('#button_b').style.backgroundColor = 'red';
+    location.replace("/mangshi_Ruili_Two_Day_Tour.html");
+});
+//瑞丽腾冲两日游跳转效果+跳转功能
+document.querySelector('#button_c').addEventListener('click', () => {
+    document.querySelector('#button_c').style.backgroundColor = 'red';
+    location.replace("/mangshi_Tengchong_Two_Day_Tour.html");
+});
+//瑞丽瑞丽腾冲五日游跳转效果+跳转功能
+document.querySelector('#button_d').addEventListener('click', () => {
+    document.querySelector('#button_d').style.backgroundColor = 'red';
+    location.replace("/mangshi_Ruili_Tengchong_Five_Day_Tour.html");
+});
